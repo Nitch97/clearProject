41f35d3d951042196ba26d8fe18eb46af52e9908
Shao Wei Su <shaoweisu@Shaos-iMac.local>
2017-09-30 16:29:02 +0800

    Initial Commit

---
+++ clearProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  clearProject
+//
+//  Created by Shao Wei Su on 2017/9/30.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ clearProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  clearProject
+//
+//  Created by Shao Wei Su on 2017/9/30.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
